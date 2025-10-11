9f6f6c0dd95de688b922c05b568262f08aa50f7d
Ilya <ilyai.2020@mail.ru>
2025-10-11 15:02:29 +0300

    added for loop in run.sh

--- task_4_files/run.sh
+++ task_4_files/run.sh
@@ -1,25 +1,6 @@
 clear&&clear
 
-number_of_schema=1
-./task_4 ${number_of_schema}
-number_of_schema=${number_of_schema} bash make_graph.sh 
-
-number_of_schema=2
-./task_4 ${number_of_schema}
-number_of_schema=${number_of_schema} bash make_graph.sh 
-
-number_of_schema=3
-./task_4 ${number_of_schema}
-number_of_schema=${number_of_schema} bash make_graph.sh 
-
-number_of_schema=4
-./task_4 ${number_of_schema}
-number_of_schema=${number_of_schema} bash make_graph.sh 
-
-number_of_schema=5
-./task_4 ${number_of_schema}
-number_of_schema=${number_of_schema} bash make_graph.sh 
-
-number_of_schema=6
-./task_4 ${number_of_schema}
-number_of_schema=${number_of_schema} bash make_graph.sh 
+for number_of_schema in {1..6}; do
+    ./task_4 ${number_of_schema}
+    number_of_schema=${number_of_schema} bash make_graph.sh 
+done
