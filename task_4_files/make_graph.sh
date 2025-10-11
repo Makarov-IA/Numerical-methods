8a8b52f7fba64848e0655aa2a3d9c04631e1940e
Ilya <ilyai.2020@mail.ru>
2025-10-11 14:59:10 +0300

    made folder

--- task_4_files/make_graph.sh
+++ task_4_files/make_graph.sh
@@ -1,13 +1,14 @@
 gnuplot -e "
+
 set term pngcairo size 1200,700 enhanced font ',14';
-set output 'plot_schema_${number_of_schema}.png';
+set output 'plots/plot_schema_${number_of_schema}.png';
 set grid;
 
 set xlabel 'ln(1/h)';
 set ylabel 'ln(1/|y(1)-y_{n}|)';
 
 set xzeroaxis; set yzeroaxis;
-set xrange [0:*]; set yrange [0:*];
+set xrange [2:*]; set yrange [0:*];
 
 set title 'schema=${number_of_schema}';
 plot \
@@ -17,5 +18,5 @@ plot \
   'data_graph/tech_graph_${number_of_schema}_3.txt' using 1:2 with lines lw 2 title 'A=100',\
   x + 20 t 'y=x+C' lc rgb 'black',\
   2*x + 20 t 'y=2*x+C' lc rgb 'black',\
-  3*x + 20 t 'y=3*x+C' lc rgb 'black';\
+  3*x + 20 t 'y=3*x+C' lc rgb 'black';
 "
