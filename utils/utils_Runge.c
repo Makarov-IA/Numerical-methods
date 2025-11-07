90fa476483cbd2711e538ec47a50b0ddd2e81fd6
Ilya <ilyai.2020@mail.ru>
2025-11-07 15:39:39 +0300

    up

--- utils/utils_Runge.c
+++ utils/utils_Runge.c
@@ -6,11 +6,11 @@
 #include "utils_Runge.h"
 
 void set_k(double xn, double yn, double *k1, double *k2, double *k3, \
-    double *k4, double h, double (*f) (double, double)) {
-    *k1 = h*f(xn, yn);
-    *k2 = h*f(xn + 0.5*h, yn + 0.5*(*k1));
-    *k3 = h*f(xn + 0.5*h, yn + 0.5*(*k2));
-    *k4 = h*f(xn + h, yn + (*k3));
+    double *k4, double h, double (*f) (double, double, double*), double *additional_args_to_function) {
+    *k1 = h*f(xn, yn, additional_args_to_function);
+    *k2 = h*f(xn + 0.5*h, yn + 0.5*(*k1), additional_args_to_function);
+    *k3 = h*f(xn + 0.5*h, yn + 0.5*(*k2), additional_args_to_function);
+    *k4 = h*f(xn + h, yn + (*k3), additional_args_to_function);
 }
 
 double delta_y(double k1, double k2, double k3, double k4) {
@@ -38,28 +38,48 @@ int get_step(double error, double thr_error, int K, double *h, double y_small, d
     }
 }
 
-void make_step(double *xn, double *yn, double h, double (*f) (double, double)) {
+void make_step(double *xn, double *yn, double h, double (*f) \
+(double, double, double*), double *additional_args_to_function) {
     double k1, k2, k3, k4;
-    set_k(*xn, *yn, &k1, &k2, &k3, &k4, h, f);
+    set_k(*xn, *yn, &k1, &k2, &k3, &k4, h, f, additional_args_to_function);
     *yn = *yn + delta_y(k1, k2, k3, k4);
     *xn = *xn + h;
 }
 
-double test_function_1 (double xn, double yn) {
+double test_function_1 (double xn, double yn, double *additional_args_to_function) {
     (void) yn;
+    (void) additional_args_to_function;
     return -sin(xn);
 }
 
-double test_function_sol_1 (double xn) {
+double test_function_sol_1 (double xn, double *additional_args_to_function) {
+    (void) additional_args_to_function;
     return cos(xn);
 }
 
-double test_function_2 (double xn, double yn) {
+double test_function_2 (double xn, double yn, double *additional_args_to_function) {
     (void) yn;
+    (void) additional_args_to_function;
     return 3*pow(xn,2) + 4*xn + 3;
 }
 
-double test_function_sol_2 (double xn) {
+double test_function_sol_2 (double xn, double *additional_args_to_function) {
+    (void) additional_args_to_function;
     return pow(xn,3) + 2*pow(xn,2) + 3*xn + 4;
+}
+
+double test_function_3 (double xn, double yn, double *additional_args_to_function) {
+    (void) yn;
+    double A = additional_args_to_function[0];
+    double alpha = additional_args_to_function[1];
+    double beta = additional_args_to_function[2];
+    return A*beta*pow((xn-alpha),beta-1)*exp(A*pow((xn-alpha),beta));
+}
+
+double test_function_sol_3 (double xn, double *additional_args_to_function) {
+    double A = additional_args_to_function[0];
+    double alpha = additional_args_to_function[1];
+    double beta = additional_args_to_function[2];
+    return exp(A*pow((xn-alpha),beta));
 }
 
--- utils/utils_Runge.h
+++ utils/utils_Runge.h
@@ -1,13 +1,27 @@
 #ifndef UTILS_RUNGE_H
 #define UTILS_RUNGE_H
 
-void set_k(double xn, double yn, double *k1, double *k2, double *k3, double *k4, double h, double (*f) (double, double));
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <math.h>
+#include <stdbool.h>
+#include "utils_Runge.h"
+
+void set_k(double xn, double yn, double *k1, double *k2, double *k3, \
+    double *k4, double h, double (*f) (double, double, double*), \
+    double *additional_args_to_function);
 double delta_y(double k1, double k2, double k3, double k4);
-int get_step(double error, double thr_error, int K, double *h, double y_small, double *y, double *x);
-void make_step(double *xn, double *yn, double h, double (*f) (double, double));
-double test_function_1 (double xn, double yn);
-double test_function_sol_1 (double xn);
-double test_function_2 (double xn, double yn);
-double test_function_sol_2 (double xn);
+int get_step(double error, double thr_error, int K, \
+    double *h, double y_small, double *y, double *x);
+void make_step(double *xn, double *yn, double h, double (*f) \
+(double, double, double*), double *additional_args_to_function);
+double test_function_1 (double xn, double yn, double *additional_args_to_function);
+double test_function_sol_1 (double xn, double *additional_args_to_function);
+double test_function_2 (double xn, double yn, double *additional_args_to_function);
+double test_function_sol_2 (double xn, double *additional_args_to_function);
+double test_function_3 (double xn, double yn, double *additional_args_to_function);
+double test_function_sol_3 (double xn, double *additional_args_to_function);
+
 
 #endif
