0da9cda0a3b3a2568009a716046263c0a5e8f03c
Ilya <ilyai.2020@mail.ru>
2025-11-22 18:02:04 +0300

    changed colores

--- task_6_files/make_plot.sh
+++ task_6_files/make_plot.sh
@@ -11,6 +11,6 @@ set xzeroaxis; set yzeroaxis;
 
 set title 'set\_number=${set_number} N=${N}';
 plot \
-  'data_graph/${dir}/${set_number}.txt' using 1:2 with lines lw 2 title 'Solution',\
-  'data_graph/${dir}/${set_number}.txt' using 1:3 with lines lw 1 dt 2 title 'Theoretical solution',\
+  'data_graph/${dir}/${set_number}.txt' using 1:2 with lines lw 4 lc rgb 'blue' title 'Solution',\
+  'data_graph/${dir}/${set_number}.txt' using 1:3 with lines lw 3 dt 2 lc rgb 'red' title 'Theoretical solution',\
 "
