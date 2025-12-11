77d61d75003694ff7bd59eca6a85d4221002f8c3
Ilya <ilyai.2020@mail.ru>
2025-12-12 00:37:21 +0300

    up

--- task_7_files/make_plot.sh
+++ task_7_files/make_plot.sh
@@ -1,18 +1,29 @@
+diff=1e+0
 gnuplot -e "
 
-set term pngcairo size 1200,800 enhanced font ',12';
+set term pngcairo size 1600,1200 enhanced font ',12';
 set output 'plots/${set_number}.png';
-set grid;
 
+set grid;
 set xlabel 'x';
 set ylabel 't';
 set zlabel 'u';
-
-set title 'set\_number=${set_number} Nx=${number_of_points_x} Nt=${number_of_points_t}';
 set hidden3d;
 set key outside;
 
-splot \
-  'data_graph/${set_number}.txt' using 2:1:3 with lines lc rgb 'blue' title 'u(t,x)', \
-  'data_graph/${set_number}.txt' using 2:1:4 with lines lc rgb 'red' dt 2 title 'theoretical';
+set multiplot layout 2,2 title 'set_number=${set_number} Nx=${number_of_points_x} Nt=${number_of_points_t}' font ',14';
+
+set view 60,40; splot 'data_graph/${set_number}.txt' using 2:1:(\$3) with points pt 7 ps 0.8 lc rgb 'blue' title 'u(t,x)', \
+                            'data_graph/${set_number}.txt' using 2:1:(\$4+${diff}) with points pt 7 ps 0.8 lc rgb 'red' title 'theoretical (+eps)';
+
+set view 30,120; splot 'data_graph/${set_number}.txt' using 2:1:(\$3) with points pt 7 ps 0.8 lc rgb 'blue' title 'u(t,x)', \
+                              'data_graph/${set_number}.txt' using 2:1:(\$4+${diff}) with points pt 7 ps 0.8 lc rgb 'red' title 'theoretical (+eps)';
+
+set view 80,220; splot 'data_graph/${set_number}.txt' using 2:1:(\$3) with points pt 7 ps 0.8 lc rgb 'blue' title 'u(t,x)', \
+                              'data_graph/${set_number}.txt' using 2:1:(\$4+${diff}) with points pt 7 ps 0.8 lc rgb 'red' title 'theoretical (+eps)';
+
+set view 20,310; splot 'data_graph/${set_number}.txt' using 2:1:(\$3) with points pt 7 ps 0.8 lc rgb 'blue' title 'u(t,x)', \
+                              'data_graph/${set_number}.txt' using 2:1:(\$4+${diff}) with points pt 7 ps 0.8 lc rgb 'red' title 'theoretical (+eps)';
+
+unset multiplot;
 "
