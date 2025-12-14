02a10f11bcfa2726facb2186a3f6249e953d0c8c
Ilya <ilyai.2020@mail.ru>
2025-12-14 17:09:58 +0300

    up

--- task_7_files/run.sh
+++ task_7_files/run.sh
@@ -8,10 +8,10 @@ for set_number in 1 2 3 4; do
     # явная схема
     ./task_7 1 ${set_number} ${T} ${number_of_points_x} ${number_of_points_t}
     mv data_graph/${set_number}.txt data_graph/${set_number}-explicit.txt
-    # set_number=${set_number}-explicit number_of_points_x=${number_of_points_x} number_of_points_t=${number_of_points_t} bash make_plot.sh
+    set_number=${set_number}-explicit number_of_points_x=${number_of_points_x} number_of_points_t=${number_of_points_t} bash make_plot.sh
 
     # неявная схема
     ./task_7 2 ${set_number} ${T} ${number_of_points_x} ${number_of_points_t}
     mv data_graph/${set_number}.txt data_graph/${set_number}-implicit.txt
-    # set_number=${set_number}-implicit number_of_points_x=${number_of_points_x} number_of_points_t=${number_of_points_t} bash make_plot.sh
+    set_number=${set_number}-implicit number_of_points_x=${number_of_points_x} number_of_points_t=${number_of_points_t} bash make_plot.sh
 done
